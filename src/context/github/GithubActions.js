85f3a439e3800689959577fb80ccb25418e546a2
Rob <babblespring@gmail.com>
2022-11-23 19:56:05 -1000

    New Commit

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,28 +1,31 @@
-import axios from "axios";
-
+import axios from 'axios'
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
+console.log(GITHUB_TOKEN)
 const github = axios.create({
-    baseURL: GITHUB_URL,
-    headers: {Authorization: `token ${GITHUB_TOKEN}`}
+    baseURL: `${GITHUB_URL}`,
+    headers: {
+        Authorization: `'Bearer ${GITHUB_TOKEN}'`
+    }
 })
 
+// Get search results
 export const searchUsers = async (text) => {
-
     const params = new URLSearchParams({
-        q: text
+        q: text,
     })
 
-const response = await github.get(`/search/users?${params}`)
-return response.data.items
+    const response = await github.get(`/search/users?${params}`)
+    return response.data.items
 }
 
-export const getUserAndRepos = async(login) => {
-    const [user,repos] = await Promise.all([
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+    const [user, repos] = await Promise.all([
         github.get(`/users/${login}`),
-        github.get(`/users/${login}/repos`)
+        github.get(`/users/${login}/repos`),
     ])
 
-    return { user:user.data,repos:repos.data}
+    return { user: user.data, repos: repos.data }
 }
