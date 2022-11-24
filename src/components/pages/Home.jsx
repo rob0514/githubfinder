6ef981057507d4ad7be1cf7efab88017ca26d25d
Rob <babblespring@gmail.com>
2022-11-23 17:51:24 -1000

    New Commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../users/UserResults";
+import UserSearch from "../users/UserSearch";
+const Home = (props) => {
+    return (
+        <>
+            <UserSearch />
+            <UserResults />
+        </>
+    );
+}
+
+export default Home;
